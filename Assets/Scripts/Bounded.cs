6bcf9afe1f527d0ac77bf733b1a9523de1aab2ae
Athir Saleem <noc7c9@gmail.com>
2016-09-10 19:04:07 +1000

    Collisions

---
+++ Assets/Scripts/Bounded.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounded : MonoBehaviour {
+
+    public bool boundX = true;
+    public float minX = -500;
+    public float maxX = 500;
+
+    public bool boundY = false;
+    public float minY = -500;
+    public float maxY = 500;
+
+    public bool boundZ = true;
+    public float minZ = -500;
+    public float maxZ = 500;
+
+    void FixedUpdate() {
+        Vector3 pos = transform.position;
+        pos.x = boundX ? Mathf.Clamp(pos.x, minX, maxX) : pos.x;
+        pos.y = boundY ? Mathf.Clamp(pos.y, minY, maxY) : pos.y;
+        pos.z = boundZ ? Mathf.Clamp(pos.z, minZ, maxZ) : pos.z;
+        transform.position = pos;
+    }
+
+}
---
+++ Assets/Scripts/NoTumble.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoTumble : MonoBehaviour {
+
+    public float drag = 0.5f;
+
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+        rb.velocity = drag * rb.velocity;
+        rb.angularVelocity = drag * rb.angularVelocity;
+	}
+
+}
